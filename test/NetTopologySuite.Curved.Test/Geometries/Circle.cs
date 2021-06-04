f0ee999dbb8655ce6f157e5d3d59b8c6630ea098
Felix Obermaier <felix.obermaier@netcologne.de>
2021-06-04 15:05:00 +0200

    Improve unit test for CircularArc
    * Add Circle utitlity class
    * Fix Center computation

---
+++ test/NetTopologySuite.Curved.Test/Geometries/Circle.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using NetTopologySuite.Algorithm;
+using NetTopologySuite.Geometries;
+
+namespace NetTopologySuite.Test.Geometries
+{
+    /// <summary>
+    /// Utility class for creating and testing <see cref="CircularArc"/>s
+    /// </summary>
+    internal class Circle
+    {
+        private const double Tolerance = 1E-10d;
+
+        /// <summary>
+        /// Creates a circle around <see cref="Coordinate"/>(0, 0) with the given <paramref name="radius"/>
+        /// </summary>
+        /// <param name="radius">The radius of the circle</param>
+        public Circle(double radius)
+            :this(0, 0, radius)
+        {}
+
+        /// <summary>
+        /// Creates a circle around <see cref="Coordinate"/>(<paramref name="x"/>, <paramref name="y"/>)
+        /// with the given <paramref name="radius"/>
+        /// </summary>
+        /// <param name="x">The x-ordinate value of the <see cref="Center"/></param>
+        /// <param name="y">The y-ordinate value of the <see cref="Center"/></param>
+        /// <param name="radius">The radius of the circle</param>
+        public Circle(double x, double y, double radius)
+            : this(new Coordinate(x, y), radius)
+        {
+        }
+
+        /// <summary>
+        /// Creates a circle around <paramref name="center"/> with the given <paramref name="radius"/>
+        /// </summary>
+        /// <param name="center">The  <see cref="Center"/></param>
+        /// <param name="radius">The radius of the circle</param>
+        public Circle(Coordinate center, double radius)
+        {
+            Center = center.Copy();
+            Radius = radius;
+        }
+
+
+        /// <summary>
+        /// Gets a value indicating the center of the circle
+        /// </summary>
+        public Coordinate Center { get; }
+
+        /// <summary>
+        /// Gets a value indicating the radius of the circle
+        /// </summary>
+        public double Radius { get; }
+
+        /// <summary>
+        /// Get a circular arc off of this circle
+        /// </summary>
+        /// <param name="startAngle">The start angle [degrees]</param>
+        /// <param name="endAngle">The end angle [degrees]</param>
+        /// <returns>A circular arc</returns>
+        public CircularArc GetCircularArc(double startAngle, double endAngle)
+        {
+            double midAngle = startAngle + (endAngle - startAngle) * 0.5;
+            return GetCircularArc(startAngle, midAngle, endAngle);
+        }
+
+        /// <summary>
+        /// Get a circular arc off of this circle
+        /// </summary>
+        /// <param name="startAngle">The start angle [degrees]</param>
+        /// <param name="midAngle">The start angle [degrees]</param>
+        /// <param name="endAngle">The end angle [degrees]</param>
+        /// <returns>A circular arc</returns>
+        public CircularArc GetCircularArc(double startAngle, double midAngle, double endAngle)
+        {
+            // Normalize arguments
+            startAngle = AngleUtility.ToRadians(startAngle);
+            midAngle = AngleUtility.ToRadians(midAngle);
+            endAngle = AngleUtility.ToRadians(endAngle);
+
+            double minAngle = startAngle <= endAngle ? startAngle : endAngle;
+            double maxAngle = startAngle > endAngle ? startAngle : endAngle;
+
+            // Check midAngle
+            if (midAngle < minAngle || maxAngle < midAngle)
+                throw new ArgumentOutOfRangeException(nameof(midAngle));
+
+            // Full Circle
+            if (Math.Abs(AngleUtility.Normalize(startAngle) - AngleUtility.Normalize(endAngle)) < 1E-10)
+            {
+                var p0 = GetPointAtAngle(0);
+                return new CircularArc(p0, Center, p0.Copy());
+            }
+
+            // Arc
+            var coordinates = new[]
+            {
+                GetPointAtAngle(startAngle),
+                GetPointAtAngle(midAngle),
+                GetPointAtAngle(endAngle)
+            };
+
+            var sequence = NtsGeometryServices.Instance.DefaultCoordinateSequenceFactory.Create(coordinates);
+            return new CircularArc(sequence, 0);
+        }
+
+        /// <summary>
+        /// Get a point on this circle specified by <paramref name="angle"/>
+        /// </summary>
+        /// <param name="angle"></param>
+        /// <returns></returns>
+        private Coordinate GetPointAtAngle(double angle)
+        {
+            return new Coordinate(
+                Center.X + Math.Cos(angle) * Radius,
+                Center.Y + Math.Sin(angle) * Radius);
+        }
+
+        /// <summary>
+        /// Predicate to test of a circular arc is part of this circle
+        /// </summary>
+        /// <param name="arc">The arc</param>
+        /// <param name="reason">A reason, why it is not on the circle</param>
+        /// <returns><c>true</c> if it is on the circle</returns>
+        public bool IsOnCircle(CircularArc arc, out string reason)
+        {
+            return IsOnCircle(arc, Tolerance, out reason);
+        }
+
+        public bool IsOnCircle(CircularArc arc, double tolerance, out string reason)
+        {
+            reason = null;
+            if (arc.Center.Distance(Center) > tolerance)
+            {
+                reason = "Arc's center does not match this circle's center.";
+                return false;
+            }
+
+            if (arc.P0.Distance(Center) - Radius > tolerance)
+            {
+                reason = "CircularArc.P0's distance to this circle's center is bigger than its radius.";
+                return false;
+            }
+
+            if (arc.P1.Distance(Center) - Radius > tolerance)
+            {
+                reason = "CircularArc.P0's distance to this circle's center is bigger than its radius.";
+                return false;
+            }
+
+            if (arc.P2.Distance(Center) - Radius > tolerance)
+            {
+                reason = "CircularArc.P2's distance to this circle's center is bigger than its radius.";
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool IsOnCircle(IList<Coordinate> arc, double tolerance, out string reason)
+        {
+            reason = null;
+            for (int i = 0; i < arc.Count; i++)
+            {
+                if (arc[i].Distance(Center) - Radius > tolerance)
+                {
+                    reason = $"Distance of flattened arc's coordinate {i}' to this circle's center is bigger than its radius.";
+                    return false;
+                }
+
+            }
+
+            return true;
+        }
+        public override string ToString()
+        {
+            return $"Circle[{Center}, radius={Radius}]";
+        }
+    }
+}
--- test/NetTopologySuite.Curved.Test/Geometries/CircularArcTest.cs
+++ test/NetTopologySuite.Curved.Test/Geometries/CircularArcTest.cs
@@ -1,5 +1,5 @@
 using System;
-using System.Xml;
+using NetTopologySuite.Algorithm;
 using NetTopologySuite.Geometries;
 using NUnit.Framework;
 
@@ -7,6 +7,9 @@ namespace NetTopologySuite.Test.Geometries
 {
     public class CircularArcTest
     {
+        private const double AngleTolerance = 1E-5;
+        private const double LengthTolerance = 1E-5;
+        private const double DistanceTolerance = 1.4142135623730951d * LengthTolerance;
 
         [TestCase(0, 10d, 7.0710678118654755d, 7.0710678118654755d, 10d, 0d, 0d, 0d, 10d, -Math.PI / 2d)]
         [TestCase(10d, 0d, 0d, 10d, -10d, 0d, 0d, 0d, 10d, Math.PI)]
@@ -36,7 +39,7 @@ namespace NetTopologySuite.Test.Geometries
                 Assert.That(ca.Length, Is.EqualTo(Math.Sqrt(dx*dx+dy*dy)).Within(1e-10));
             }
             else
-                Assert.That(ca.Length, Is.EqualTo(angle * radius).Within(1e-7));
+                Assert.That(ca.Length, Is.EqualTo(Math.Abs(angle * radius)).Within(1e-7));
         }
 
         [TestCase(0, 10d, 7.0710678118654755d, 7.0710678118654755d, 10d, 0d, 0d, 10000d)]
@@ -66,5 +69,128 @@ namespace NetTopologySuite.Test.Geometries
             Assert.That(geom.IsCoordinate(ca.P1));
             Assert.That(geom.IsCoordinate(ca.P2));
         }
+
+        [Test]
+        public void TestLine()
+        {
+            
+            var ca = new CircularArc(new Coordinate(0, 0), new Coordinate(0, 10), new Coordinate(0, 20));
+            Assert.That(ca.Radius, Is.EqualTo(CircularArc.CollinearRadius));
+            Assert.That(ca.Angle, Is.EqualTo(CircularArc.CollinearAngle));
+            Assert.That(ca.Center, Is.EqualTo(new LineSegment(ca.P0, ca.P2).MidPoint));
+
+            var flattened = CheckFlattened(ca, points: new[] {ca.P0, ca.P1, ca.P2}, length:20d,
+                lengthTolerance: 0d, distanceTolerance: 0d);
+
+            CheckEnvelopesMatch(ca.Envelope, GetEnvelope(flattened));
+        }
+
+        [Test]
+        public void TestIdenticalPoints()
+        {
+            var center = new Coordinate(1, 1);
+            var ca = new CircularArc(new Coordinate(1, 1), new Coordinate(1, 1), new Coordinate(1, 1));
+            Assert.That(ca.Radius, Is.EqualTo(0d));
+            Assert.That(ca.Angle, Is.EqualTo(0d));
+            Assert.That(ca.Center, Is.EqualTo(center));
+
+            var flattened = CheckFlattened(ca, length: 0d, points: new[] {ca.P0, ca.P1, ca.P2}, lengthTolerance: 0d);
+            CheckEnvelopesMatch(ca.Envelope, GetEnvelope(flattened));
+        }
+
+        [TestCase(50, 30, 1)]
+        [TestCase(50, 30, 0.5)]
+        [TestCase(50, 30, 0.1)]
+        [TestCase(100, 45, 0.05)]
+        public void TestTiny(double radius, double angle, double opening)
+        {
+            var circle = new Circle(11, 12, radius);
+            var arc1 = circle.GetCircularArc(angle, angle + opening);
+            double length = AngleUtility.NormalizePositive(AngleUtility.ToRadians(opening)) * radius;
+
+
+            Assert.That(arc1.Center.Distance(circle.Center), Is.LessThanOrEqualTo(25 * DistanceTolerance));
+            Assert.That(arc1.Radius, Is.EqualTo(circle.Radius).Within(25 * LengthTolerance));
+            Assert.That(arc1.Angle, Is.EqualTo(AngleUtility.ToRadians(opening)).Within(AngleTolerance));
+            Assert.That(arc1.Length, Is.EqualTo(length).Within(LengthTolerance));
+            Assert.That(circle.IsOnCircle(arc1, DistanceTolerance, out string reason), reason);
+
+            var flattened = CheckFlattened(arc1, length / 10);
+            Assert.That(circle.IsOnCircle(flattened, DistanceTolerance, out reason), reason);
+
+            var arc2 = circle.GetCircularArc(angle + opening, angle);
+            Assert.That(arc2.Center.Distance(circle.Center), Is.LessThanOrEqualTo(25 * DistanceTolerance));
+            Assert.That(arc2.Radius, Is.EqualTo(circle.Radius).Within(25 * LengthTolerance));
+            Assert.That(arc2.Angle, Is.EqualTo(AngleUtility.ToRadians(-opening)).Within(AngleTolerance));
+            Assert.That(arc1.Length, Is.EqualTo(length).Within(LengthTolerance));
+            Assert.That(circle.IsOnCircle(arc2, DistanceTolerance, out reason), reason);
+
+            flattened = CheckFlattened(arc1, length / 10);
+            Assert.That(circle.IsOnCircle(flattened, DistanceTolerance, out reason), reason);
+        }
+
+
+        private static CoordinateList CheckFlattened(CircularArc actual, double arcStepLength = 0d, PrecisionModel precisionModel = null,
+            int? numPoints = null, double? length = null, Coordinate[] points = null,
+            double distanceTolerance = DistanceTolerance, double lengthTolerance = LengthTolerance)
+        {
+            var res = actual.Flatten(arcStepLength, precisionModel);
+
+            if (points != null && !numPoints.HasValue)
+                numPoints = points.Length;
+
+            if (points != null && points.Length != numPoints)
+                throw new ArgumentException(nameof(numPoints));
+
+            if (numPoints.HasValue)
+                Assert.That(res.Count, Is.EqualTo(numPoints.Value), "Number of points");
+
+            if (points != null)
+            {
+                for (int i = 0; i < numPoints.Value; i++)
+                    Assert.That(res[i].Distance(points[i]), Is.EqualTo(0).Within(distanceTolerance));
+            }
+
+            if (length.HasValue)
+                Assert.That(GetLength(res), Is.EqualTo(length.Value).Within(lengthTolerance));
+
+            return res;
+        }
+
+        private static void CheckEnvelopesMatch(Envelope actual, Envelope expected, double tolerance = DistanceTolerance)
+        {
+            if (expected.IsNull)
+                Assert.That(actual.IsNull, "actual.IsNull");
+
+            Assert.That(actual.MinX, Is.EqualTo(expected.MinX).Within(tolerance), "actual.MinX");
+            Assert.That(actual.MaxX, Is.EqualTo(expected.MaxX).Within(tolerance), "actual.MaxX");
+            Assert.That(actual.MinY, Is.EqualTo(expected.MinY).Within(tolerance), "actual.MinY");
+            Assert.That(actual.MaxY, Is.EqualTo(expected.MaxY).Within(tolerance), "actual.MaxY");
+        }
+
+        private static Envelope GetEnvelope(CoordinateList flattened)
+        {
+            var res = new Envelope();
+            if (flattened == null || flattened.Count == 0)
+                return res;
+
+            for (int i = 0; i < flattened.Count; i++)
+                res.ExpandToInclude(flattened[i]);
+
+            return res;
+        }
+
+        private static double GetLength(CoordinateList flattened)
+        {
+            double res = 0d;
+            if (flattened == null || flattened.Count <= 1)
+                return res;
+
+            for (int i = 1; i < flattened.Count; i++)
+                res += flattened[i - 1].Distance(flattened[i]);
+
+            return res;
+        }
+
     }
 }
